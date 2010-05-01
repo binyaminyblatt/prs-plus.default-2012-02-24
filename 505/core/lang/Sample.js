4bf22bcc9412434aab1eeba81b6eee07eb9a00e6
kartu <kartu@localhost>
2010-05-01 08:03:39 +0200

    Renamed 'Viewer settings' to 'Book Viewer Settings'

--- 505/core/lang/Sample.js
+++ 505/core/lang/Sample.js
@@ -265,7 +265,7 @@ return {
 		FUNC_X_SETTINGS: FUNC_X_SETTINGS,
 		NODE_PRSP_SETTINGS:  "PRS+ Settings",
 		GROUP_MENU_TITLE: "Menu Settings",
-		GROUP_VIEWER_TITLE: "Viewer Settings"
+		GROUP_VIEWER_TITLE: "Book Viewer Settings"
 	},
 
 	CoreLang: {
