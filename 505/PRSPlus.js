eb0da2fefe50ed9d3445e624cd386f350cb77f26
kartu <kartu@localhost>
2010-05-10 13:54:40 +0200

    Added userDictionaryPath to config Moved prspCoreFile and prspAddonsFile to allow modifications via userconfig (vars are named coreFile and addonsFile respectively) If coreFile/addonsFile is a folder, .js files inside it will be combined and called

--- 505/PRSPlus.js
+++ 505/PRSPlus.js
@@ -1,3 +1,11 @@
+// Description: PRS+ bootstrap script
+// Author: kartu
+//
+// History:
+//	2010-05-10 kartu - Added userDictionaryPath to config
+//				Moved prspCoreFile and prspAddonsFile to allow modifications via userconfig
+//				If prspCoreFile/prspAddonsFile is a folder, .js files inside it will be combined into one and called 
+
 // If not in safe mode  (USB was connected during startup) not initializing PRS+
 if (!FileSystem.getFileInfo(System.applyEnvironment("[prspSafeModeFile]"))) {
 	var bootLog;
@@ -9,8 +17,14 @@ if (!FileSystem.getFileInfo(System.applyEnvironment("[prspSafeModeFile]"))) {
 			corePath: System.applyEnvironment("[prspCorePath]"),
 			addonsPath: System.applyEnvironment("[prspAddonsPath]"),
 			settingsPath: System.applyEnvironment("[prspSettingsPath]"),
-			userCSSPath: System.applyEnvironment("[prspUserCSSPath]")
+			userCSSPath: System.applyEnvironment("[prspUserCSSPath]"),
+			userDictionaryPath: System.applyEnvironment("[userDictionaryPath]"),
+			coreFile: System.applyEnvironment("[prspCoreFile]"),
+			addonsFile: System.applyEnvironment("[prspAddonsFile]")
 		};
+		
+		// Bootstrap logger
+		//
 		bootLog = function(msg) {
 			if (config.defaultLogLevel === "none") {
 				return;
@@ -20,7 +34,42 @@ if (!FileSystem.getFileInfo(System.applyEnvironment("[prspSafeModeFile]"))) {
 			s.writeLine(msg);
 			s.close();
 		};
-		var Core = {config: config};
+
+		// Checks if string ends with given postfix
+		//		
+		var endsWith = function(str, postfix) {
+			return str.lastIndexOf(postfix) === str.length - postfix.length;
+		};
+		
+		// Returns array of files with given extension sorted by name
+		//
+		var listFiles = function(path, ext) {
+			try {
+				var iterator = new FileSystem.Iterator(path);
+				try {
+					var items = [];
+					var item;
+					while (item = iterator.getNext()) {
+						if (item.type == "file") {
+							var p = item.path;
+							if (ext === undefined || endsWith(p, ext)) {
+								items.push(p);
+							}
+						}
+					}
+					items.sort();
+					return items;
+				} finally {
+					iterator.close();
+				}
+			} catch (e) {
+				bootLog("Error in list files, listing folder " + path + ": " + e);
+			}
+			return [];
+		};
+		
+		// Loads file contents
+		//
 		var getFileContent = function(path) {
 			try {
 				var f = new Stream.File(path, 2);
@@ -32,85 +81,47 @@ if (!FileSystem.getFileInfo(System.applyEnvironment("[prspSafeModeFile]"))) {
 			}
 			return "";
 		};
+		
+		// Loads file, or, if path points to a folder, combined content of the files in folder, with extention <ext>
+		//
+		var getFileContentEx = function(path, ext) {
+			var info = FileSystem.getFileInfo(path);
+			if (info && info.type == "directory") {
+				var files = listFiles(path, ext);
+				var result = "";
+				for (var i = 0, n = files.length; i < n; i++) {
+					result = result + getFileContent(files[i]);
+				}
+				return result;
+			} 
+			return getFileContent(path);
+		}
 
 		// Load user config
 		var userConfig = System.applyEnvironment("[prspUserConfig]");
-		var debugMode = false;
 		if (FileSystem.getFileInfo(userConfig)) {
-			debugMode = true;
 			var f = new Function("config", getFileContent(userConfig));
 			f(config);
 			delete f;
 		}
 
-		// In debug mode, need to combine files manually, otherwise call prspFile, which was created by bulid script
-		//
-		if (debugMode) {
-			var endsWith = function(str, postfix) {
-				return str.lastIndexOf(postfix) === str.length - postfix.length;
-			};
+		var Core = {config: config};
+		
+		// Call core (there seems to be 100k limitation on javascript size, that's why it's split from addons)
+		var coreCode = getFileContentEx(config.coreFile, ".js");
+		var core = new Function("Core", coreCode);
+		core(Core);
+		delete core;
+		delete coreCode;
 
-			var listFiles = function(path, ext) {
-				try {
-					var iterator = new FileSystem.Iterator(path);
-					try {
-						var items = [];
-						var item;
-						while (item = iterator.getNext()) {
-							if (item.type == "file") {
-								var p = item.path;
-								if (ext === undefined || endsWith(p, ext)) {
-									items.push(p);
-								}
-							}
-						}
-						items.sort();
-						return items;
-					} finally {
-						iterator.close();
-					}
-				} catch (e) {
-					bootLog("Error in list files, listing folder " + path + ": " + e);
-				}
-				return [];
-			};
+		// Call addons
+		var addonCode = getFileContentEx(config.addonsFile, ".js");
+		var log = Core.log.getLogger("addons");
+		var addons = new Function("Core,log,tmp", addonCode);
+		addons(Core, log, undefined);
+		delete addonCode;
+		delete addons;
 
-			// Combine & call core files (there seems to be 100k limitation on javascript size, that's why it's split from addons)
-			var corePath = config.corePath;
-			var coreFiles = listFiles(corePath, ".js");
-			var coreCode = "";
-			for (var i = 0, n = coreFiles.length; i < n; i++) {
-				coreCode = coreCode + getFileContent(corePath + coreFiles[i]);
-			}
-			var core = new Function("Core", coreCode);
-			core(Core);
-			delete core;
-			delete coreCode;
-
-			// Call addon files
-			var addonsPath = config.addonsPath;
-			var addonFiles = listFiles(addonsPath, ".js");
-			var addonCode = "";
-			for (i = 0, n = addonFiles.length; i < n; i++) {
-				addonCode = addonCode + getFileContent(addonsPath + addonFiles[i]);
-			}
-			var log = Core.log.getLogger("addons");
-			var addons = new Function("Core,log,tmp", addonCode);
-			addons(Core, log, undefined);
-			delete addonCode;
-			delete addons;
-
-		} else {
-			var prspCoreFile = System.applyEnvironment("[prspCoreFile]");
-			var core = new Function("Core", getFileContent(prspCoreFile));
-			core(Core);
-			delete core;
-
-			var prspAddonsFile = System.applyEnvironment("[prspAddonsFile]");
-			var addons = new Function("Core", getFileContent(prspAddonsFile));
-			addons(Core);
-			delete addons;
-		}
 		Core.init();
 	};
 	try {
