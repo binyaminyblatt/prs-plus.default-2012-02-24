d909f32fd543e9c0f67ee3af792021ff32ab69ae
Kartu (msukhiashvili <Kartu (msukhiashvili@gmail.com)>
2010-11-03 11:31:20 +0100

    600: Added opt folder from the original image.

---
+++ firmware/600/opt/sony/ebook/bin/tinyhttp.sh
@@ -0,0 +1,59 @@
+#! /bin/sh
+
+PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sony/bin:/usr/sbin"
+LD_LIBRARY_PATH="/opt/sony/ebook/application:/lib:/usr/lib:/usr/local/sony/lib"
+export PATH LD_LIBRARY_PATH
+
+#start kbook application
+/opt/sony/ebook/application/tinyhttp
+if [ $? == 0 ]; then
+	/bin/sync
+	/usr/local/sony/bin/ioctl /dev/ebklog 0x01 0x01 # log write
+	if [ -r /tmp/exitcode ]; then
+		CODE=`/bin/cat /tmp/exitcode`
+		/bin/rm /tmp/exitcode
+#		/bin/echo $CODE
+		if [ $CODE == 3 ]; then
+			NUM=`grep \"Data\" /proc/mtd | awk -F: '{print $1}' | awk -Fd '{print$2}'`
+			/usr/local/sony/bin/mkdosfs /dev/mtdblock$NUM
+			/bin/grep \"Data\" /etc/mtab > /dev/null
+			if [ $? == 0 ]; then
+#				/bin/echo reboot
+				/sbin/reboot
+			else
+#				/bin/echo restart application		
+				exec /opt/sony/ebook/bin/tinyhttp.sh
+			fi
+# E_BOOK add for Oslo SD/MS formating
+		elif [ $CODE == 4 ] ; then
+			/usr/local/sony/bin/sd_fmt /dev/sdmscard/sdmsctl0 > /dev/null
+			if [ $? == 0 ]; then
+#				/bin/echo restart application		
+# E_BOOK 2009/04/14 
+				sleep 2;
+				exec /opt/sony/ebook/bin/tinyhttp.sh
+			else
+#				/bin/echo reboot
+				/sbin/reboot
+			fi
+		elif [ $CODE == 5 ] ; then
+			/usr/local/sony/bin/sd_fmt /dev/sdmscard/sdmsctl2 > /dev/null
+			if [ $? == 0 ]; then
+#				/bin/echo restart application		
+# E_BOOK 2009/04/14 
+				sleep 2;
+				exec /opt/sony/ebook/bin/tinyhttp.sh
+			else
+#				/bin/echo reboot
+				/sbin/reboot
+			fi
+		fi
+	fi
+#	/bin/echo poweroff
+	/sbin/poweroff
+else
+	/bin/sync
+        /usr/local/sony/bin/ioctl /dev/ebklog 0x01 0x01 # log write
+#	/bin/echo reboot
+	/sbin/reboot
+fi
