b851a4df31ea2ec43cc5afa15008d03e85ecbd87
Kartu (msukhiashvili <Kartu (msukhiashvili@gmail.com)>
2011-02-08 17:42:21 +0100

    650/350: Ported to 650 (tested) and 350 (should work :))

---
+++ firmware/350/prsp.sh
@@ -0,0 +1,7 @@
+mount -t cramfs -o loop,ro /opt1/dict/prsp/resources.img /opt/sony/ebook/application/resources
+mount --bind /opt1/dict/prsp/deviceConfig.xml /opt/sony/ebook/application/deviceConfig.xml
+# Run prsp.sh shell script located on sd card,
+if [ -f /Data/database/system/PRSPlus/prsp.sh ]
+then
+	. /Data/database/system/PRSPlus/prsp.sh
+fi
