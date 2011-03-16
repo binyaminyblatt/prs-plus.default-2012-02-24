8d6926c0583545a4778e21342aa456b085287fae
Kartu (msukhiashvili <Kartu (msukhiashvili@gmail.com)>
2011-03-16 17:28:37 +0100

    600: Added fix for firmwares with alternative dictionary files.

--- firmware/600/prsp.sh
+++ firmware/600/prsp.sh
@@ -1,4 +1,11 @@
 mount -t cramfs -o loop,ro /opt1/dict/prsp/resources.img /opt/sony/ebook/application/resources
+# Fix for dictionary files
+cp /opt1/dict/prsp/kconfig_1.0.02.13180.xml /tmp
+/bin/grep -q "CDUS125D0000101Y" /opt1/dict/LIBRARY.MBF
+if [ $? == 0 ]; then
+   /bin/mount --bind /tmp/kconfig_1.0.02.13180.xml /opt/sony/ebook/application/kconfig.xml
+fi
+
 # Run prsp.sh shell script located on sd card,
 if [ -f /Data/database/system/PRSPlus/prsp.sh ]
 then
