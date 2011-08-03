e6d174738a15d345fbf4c260fbd7839be271f6ee
BenChen <ben.chenoweth@gmail.com>
2011-08-03 21:35:59 +0400

    ALL: Added Calendar and Solitaire

--- script/core/compat/350_config.js
+++ script/core/compat/350_config.js
@@ -17,7 +17,10 @@
 //      Draughts by Ben Chenoweth
 //      MineSweeper by Mark Nord / D. Shep Poor
 //      XO-Cubed by Ben Chenoweth
-//	2011-07-03 Mark Nord - Added NodeKinds.STANDBY	
+//	2011-07-03 Mark Nord - Added NodeKinds.STANDBY
+//  2011-08-03 Ben Chenoweth - ALL: Added
+//      Calendar by Ben Chenoweth
+//      Solitaire by Ben Chenoweth
 
 return {
 	// Menu icon indices 
@@ -177,7 +180,8 @@ return {
 			{ name: "collections", parent: "more" },
 			{ name: "periodicals", parent: "more" },
 			{ name: "notes", parent: "more" },
-			{ name: "Calculator", parent: "more" },	
+			{ name: "Calculator", parent: "more" },
+			{ name: "Calendar", parent: "more" },
 			{ name: "Chess", parent: "more" },
 			{ name: "Draughts", parent: "more" },
 			{ name: "FiveBalls", parent: "more" },	
@@ -185,6 +189,7 @@ return {
 			{ name: "FreeCell", parent: "more" },
 			{ name: "Mahjong", parent: "more" },
 			{ name: "MineSweeper", parent: "more" },
+			{ name: "Solitaire", parent: "more" },	
 			{ name: "Sudoku", parent: "more" },
 			{ name: "XOCubed", parent: "more" }		
 		],
