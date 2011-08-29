a356429fb3c1d1d599d4473dd8bd4a266da24e72
Mark Nord <Mark Nord@localhost>
2011-08-29 22:13:10 +0200

    x50_ViewerSettings: fix: masking of overlap works now correct in landscape-mode

--- script/addons1/x50_ViewerSetting.js
+++ script/addons1/x50_ViewerSetting.js
@@ -9,6 +9,7 @@
 // History:
 //	2011-08-21 Mark Nord - Initial version
 //	2011-08-23 Mark Nord - changed strategy to prevent dictionary (thanks quisvir)
+//	2011-08-29 Mark Nord - fix: masking of overlap works now correct in landscape-mode
 
 tmp = function() {
 
@@ -34,21 +35,211 @@ tmp = function() {
 			
 		}
 	}
+
 	// overload kbookPage.draw to peek into
-	var oldKbookPageDraw = kbook.kbookPage.draw;
+
 	var draw = function (event) {
-		try {
-			this.NotMarkOverlapArea = Core.addonByName.x50_ViewerSettings.options.NotMarkOverlapArea === "true";
-		} catch (ignore) {
+		var data, window, x, y, width, height, cache, naturalBounds, range, ratioX, ratioY, rct, state, backup, bitmap, cutout, u, xMin, xSize, yMin, ySize, size, minOverlap, maxOverlap, yMinN, yMaxN, parts, j, half, mark1, mark2, span, bounds, c, minSpan, maxSpan, i, bound, bound_y, update;
+		minOverlap =[];
+		maxOverlap =[];
+		this._isInvalidPage = false;
+		if (this.markupHelper && this.markupHelper.isActive()) {
+			this.markupHelper.draw(this, this.getWindow());
+			return;
 		}
-		
-		// call original function
-		oldKbookPageDraw.apply(this, arguments);	
-	}
+		data = this.data;
+		if (!data) {
+			return;
+		}
+		window = this.getWindow();
+		x = this.x;
+		y = this.y;
+		width = this.width;
+		height = this.height;
+		cache = this.getPageCache(this.getPage(), this.getPageOffset(), this.getPart());
+		if (cache && (!this.facing && !this.isZooming)) {
+			naturalBounds = cache.naturalBounds;
+			if (this.portMatrixReviseRatio === undefined && naturalBounds) {
+				ratioX = naturalBounds.width / this.sourcePageWidth;
+				ratioY = naturalBounds.height / this.sourcePageHeight;
+				this.portMatrixReviseRatio = Math.max(ratioX, ratioY);
+				this.portMatrixReviseX = naturalBounds.x + naturalBounds.width - (this.sourcePageWidth * this.portMatrixReviseRatio) / 2;
+				this.portMatrixReviseY = naturalBounds.y + naturalBounds.height - (this.sourcePageHeight * this.portMatrixReviseRatio) / 2;
+			}
+			if (this.marginRemove) {
+				if (!this.marginRemoveDrawRect) {
+					if (this.dynamicScrollHeight) {
+						rct = new Rectangle(naturalBounds);
+						rct.scaleToFit(0, 0, this._pageWidth, this._pageHeight);
+					}
+					else {
+						rct = new Rectangle(0, 0, this._pageWidth, this._pageHeight);
+					}
+					rct.x = naturalBounds.x + (naturalBounds.width - rct.width) / 2;
+					rct.y = naturalBounds.y + (naturalBounds.height - rct.height) / 2;
+					this.marginRemoveDrawRect = rct;
+				}
+				naturalBounds = this.marginRemoveDrawRect;
+				if (!this.isScrollView()) {
+					this.xOffset = Math.floor(naturalBounds.x + naturalBounds.width / 2 - this._pageWidth / 2);
+					this.yOffset = Math.floor(naturalBounds.y + naturalBounds.height / 2 - this._pageHeight / 2);
+				}
+			}
+			if (this.isScrollView()) {
+				this.checkNaturalInfo(naturalBounds);
+				state = this.getHalfPageWrapped();
+				this.sandbox.updateHalfPageIcon(state);
+			}
+			if (this.styleSplitPage) {
+				this.sandbox.updateSplitPageIcon(this.getSplitPageWrapped());
+				this.sandbox.showSplitHalfPageIcon(true, false);
+			}
+			else {
+				this.sandbox.showSplitHalfPageIcon(false, true);
+			}
+			this.halfPage = this.getHalfPage();
+		}
+		backup = null;
+		if (!this.monochrome.isRunning()) {
+			backup = this.execToneCurveChange(cache.bitmap);
+		}
+		Fskin.bookScroller.draw.call(this, event);
+		if (backup) {
+			cache.bitmap.close();
+			cache.bitmap = backup;
+		}
+		if (Core.addonByName.x50_ViewerSettings.options.NotMarkOverlapArea === "false") {
+			bitmap = cache.bitmap;
+			if (bitmap && !cache.error) {
+				if (!this.isZooming && (this.isScrollView() && !this.monochrome.isRunning()) ) {
+					try {
+						cutout = this.skin.cutouts[26];
+						xMin = this.getMin(false);	
+						xSize = this.getSize(false);
+						size = this.getSize(true);
+						range = this.getRange(true);
+						yMinN = this.yMinNatural;
+						yMaxN = this.yMaxNatural;
+						parts = this.partsNatural;
+						if (parts == 2) {
+							/* original code
+							minOverlap[0] = yMaxN - size;
+							maxOverlap[0] = yMinN + size; */
+							// fixed Mark Nord
+							maxOverlap[0] = yMaxN + yMinN - size; 
+							minOverlap[0] = size; 
+							}
+						else {
+							if (parts === 3) {
+								if (this.yOffset === yMinN) {
+									minOverlap[0] = Math.floor(yMaxN + yMinN - size / 2);
+									maxOverlap[0] = size; //yMinN + size;
+								}
+								else {
+									if (this.yOffset !== yMaxN - size) {
+										minOverlap[0] = Math.floor(yMaxN + yMinN - size / 2);
+										maxOverlap[0] = yMinN + size;
+										minOverlap[1] = yMaxN - size;
+										maxOverlap[1] = Math.floor(yMaxN + yMinN + size / 2);
+									}
+									else {
+										minOverlap[0] = yMaxN - size;
+										maxOverlap[0] = Math.floor(yMaxN + yMinN + size / 2);
+									}
+								}
+							}
+						}
+						j = 0;
+						while (j < minOverlap.length) {
+							half = Math.floor((minOverlap[j] + maxOverlap[j]) / 2);
+							mark1 = data.mark(this.getPage(), this.getPageOffset(), xMin, half);
+							mark2 = data.mark(this.getPage(), this.getPageOffset(), (xMin + xSize) / 2, half);
+							if (mark1 && mark2) {
+								span = new Document.Viewer.Span(mark1, mark2);
+								if (span) {
+									bounds = span.getBounds();
+									if (bounds) {
+										c = bounds.length;
+										if (c > 0) {
+											minSpan = 32767;
+											maxSpan = 0;
+											i = 0;
+											while (i < c) {
+												bound = bounds[i];
+												bound_y = bound.y + this.marginHeight;
+												if (minSpan > bound_y) {
+													minSpan = bound_y;
+												}
+												if (maxSpan < bound_y + bound.height) {
+													maxSpan = bound_y + bound.height;
+												}
+												i++;
+											}
+											update = 0;
+											if (minOverlap[j] <= minSpan && maxSpan <= maxOverlap[j]) {
+												if (this.yOffset === yMinN || (this.yOffset !== yMaxN - size && j !== 0)) {
+													if (update === 0 || minSpan < half) {
+														update = 1;
+														half = minSpan;
+													}
+												}
+												else {
+													if (update === 0 || half < maxSpan) {
+														update = 1;
+														half = maxSpan;
+													}
+												}
+											}
+										}
+										if (this.yOffset === yMinN) {
+											yMin = this.getMin(true) + half - yMinN;
+											ySize = this.getMax(true) - yMin;
+											u = 0;
+										}
+										else {
+											if (this.yOffset !== yMaxN - size) {
+												if (j === 0) {
+													yMin = this.getMin(true);
+													ySize = half - this.yOffset;
+													u = 1;
+												}
+												else {
+													yMin = this.getMin(true) + half - this.yOffset;
+													ySize = this.getMax(true) - yMin;
+													u = 0;
+												}
+											}
+											else {
+												yMin = this.getMin(true);
+												ySize = half - this.yOffset;
+												u = 1;
+											}
+										}
+										cutout.fill(window, u, 0, xMin, yMin, xSize, ySize);
+									}
+									span.close();
+								}
+							}
+							j++;
+						}
+						maxOverlap = new Array();
+					}
+					catch (e)
+					{
+						log.error("in MarkOverlap "+e, "error");	
+					}
+				}
+			}
+		}
+		this.drawError(window, x, y, cache);
+		if (this.monochrome.isRunning()) {
+			this.monochrome.setFrameInfo(this.getCurrentID(), this.mapPage(this.getPage()), this.getPageOffset(), this.getPart(), this.yOffset);
+		}
+	};
 
 	var x50_ViewerSettings = {
 		name: "x50_ViewerSettings",
-		settingsGroup: "viewer", //"advanced",
+		settingsGroup: "viewer", // "advanced",
 		optionDefs: [
 			{
 				name: "NotMarkOverlapArea",
@@ -98,8 +289,6 @@ tmp = function() {
 		/**
 		* @constructor
 		*/
-		onPreInit: function () {
-		},
 		onInit: function () {
 			kbook.kbookPage.draw = draw;
 			kbook.kbookPage.doSelectWord = doSelectWord;
