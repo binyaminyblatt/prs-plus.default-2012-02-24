e4e0dbeb0dad9d257d98694e966ec3f6984dbfa4
quisvir <quisvir@localhost>
2011-09-02 15:01:35 +0200

    Added custom contrast & brightness settings

--- script/addons1/ViewerSettings_x50.js
+++ script/addons1/ViewerSettings_x50.js
@@ -13,6 +13,7 @@
 //	2011-08-30 Mark Nord - renamed to ViewerSetting_x50; default for mask-overlap set to mask overlap
 //				keybindable action to switch gesture-pageturn on/off
 //	2011-09-01 Mark Nord - Used appropriate icons - based in Ben Chenoweth suggestion
+//  2011-09-02 quisvir - Added Custom View Settings for Restore Button
 
 tmp = function() {
 
@@ -29,7 +30,22 @@ tmp = function() {
 	var L = Core.lang.getLocalizer("ViewerSettings_x50");
 
 	// Constants
-
+	
+	pageOptionToneCurveEditorOverlayModel.initToneCurveEditor = function () {
+		var contrast, brightness;
+		contrast = parseInt(this.targetModel.doSomething('getContrast'));
+		brightness = parseInt(this.targetModel.doSomething('getBrightness'));
+		if (Core.addonByName.ViewerSettings_x50.options.BindToRestoreButton == "true") {
+		this.org_slider_1 = Core.addonByName.ViewerSettings_x50.options.CustomContrast;
+		this.org_slider_2 = Core.addonByName.ViewerSettings_x50.options.CustomBrightness;
+		}
+		else {
+		this.org_slider_1 = contrast;
+		this.org_slider_2 = brightness;
+		}
+		this.ToneUpdate(contrast, brightness);
+	};
+	
 	// overload kbook.kbookPage.doSelectWord called by kbook.kbookPage.readingTracker.doubleTap to disable Dictionary
 	var oldDoSelectWord = kbook.kbookPage.doSelectWord;
 	var doSelectWord = function (){
@@ -286,7 +302,35 @@ tmp = function() {
 					"true": L("VALUE_TRUE"),
 					"false": L("VALUE_FALSE")
 				}	
-			}			
+			},
+			{
+			groupTitle: "Custom View Settings",
+			groupIcon: "FOLDER",
+			optionDefs: [
+				{
+					name: "CustomContrast",
+					title: L("CUSTOM_CONTRAST"),
+					defaultValue: 0,
+					values: [127,120,110,100,90,80,70,60,50,45,40,35,30,25,20,15,10,5,0,-5,-10,-15,-20,-25,-30,-35,-40,-45,-50,-60,-70,-80,-90,-100,-110,-120,-127],
+				},
+				{
+					name: "CustomBrightness",
+					title: L("CUSTOM_BRIGHTNESS"),
+					defaultValue: 0,
+					values: [225,200,175,150,125,100,90,80,70,60,50,40,30,20,10,0,-10,-20,-30,-40,-50,-60,-70,-80,-90,-100,-125,-150,-175,-200,-225],
+				},
+				{
+				name: "BindToRestoreButton",
+				title: L("BIND_TO_RESTORE_BUTTON"),
+				defaultValue: "false",
+				values: ["true","false"],
+				valueTitles: {
+					"true": L("VALUE_TRUE"),
+					"false": L("VALUE_FALSE")
+				}
+				},
+			],
+			}
 		],
 		/**
 		* @constructor
@@ -301,7 +345,11 @@ tmp = function() {
 			if (ViewerSettings_x50.options.BorderColor === 'grey') 
 				{kbook.kbookPage.borderColor=Color.rgb.parse('#6D6D6D')
 			} else { kbook.kbookPage.borderColor=Color.rgb.parse('white')
-			}		
+			}
+			switch (propertyName) {
+			case "CustomContrast": case "CustomBrightness": case "BindToRestoreButton":
+			if (Core.addonByName.ViewerSettings_x50.options.BindToRestoreButton != "true") { Core.ui.showMsg(L("CUSTOM_VIEW_MSG")); }
+			}
 		},
 		actions: [{
 			name: "toggleGestureOnOff",
