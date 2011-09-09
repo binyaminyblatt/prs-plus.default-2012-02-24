b58bbd0a34b70960efa2b43106750ebe2778cc99
BenChen71 <BenChen71@localhost>
2011-09-09 11:39:38 +0400

    Calendar: Moved strings into variables to handle missing characters in the fonts on the 600

--- script/addons/Calendar/calendar.js
+++ script/addons/Calendar/calendar.js
@@ -23,6 +23,9 @@
 // 2011-09-06 Ben Chenoweth - Fixed: events text box was not updating when adding new event.
 // 2011-09-08 Ben Chenoweth - Added OK and Cancel using physical buttons for non-touch on event editor.
 // 2011-09-08 Ben Chenoweth - Removed "Space" label; fixed error with event numbers.
+// 2011-09-08 Ben Chenoweth - Replaced "Shft", "unSh", "Back" with arrow symbols.
+// 2011-09-08 Ben Chenoweth - Replaced "U", "D" with arrow symbols.
+// 2011-09-09 Ben Chenoweth - Moved strings into variables to handle missing characters in the fonts on the 600.
 
 var tmp = function () {
 	var thisDate = 1;							// Tracks current date being written in calendar
@@ -83,7 +86,24 @@ var tmp = function () {
 	var currentOffset = 0;
 	var upenabled = false;
 	var downenabled = false;
-		
+	
+	// the following strings work on all readers except the 600 because the characters are missing from the font
+	var strShift = "\u2191"; //up arrow
+	var strUnShift = "\u2193"; //down arrow
+	var strBack = "\u2190"; //left arrow
+	var strUp = "\u2191";
+	var strDown = "\u2193";
+	
+	if (!kbook.simEnviro) {
+		if (Core.config.model=="600") {
+			strShift = "Shft";
+			strUnShift = "unSh";
+			strBack = "Back";
+			strUp = "U";
+			strDown = "D";
+		}
+	}
+	
 	// variables to be saved to a file
 	target.settings = {	
 		WeekBeginsWith : "Sun"
@@ -411,10 +431,12 @@ var tmp = function () {
 		//keyboard keys in shifted form
 		this.doShift();
 		
-		//simplify some keyboard labels
-		//setSoValue(target.EVENTS_DIALOG.BACK, 'text', "\u2190"); // left arrow
+		//simplify some labels
+		setSoValue(target.EVENTS_DIALOG.BACK, 'text', strBack);
 		setSoValue(target.EVENTS_DIALOG.SPACE, 'text', "");
-		//setSoValue(target.EVENTS_DIALOG.SHIFT, 'text', "\u2193"); // down arrow	
+		setSoValue(target.EVENTS_DIALOG.SHIFT, 'text', strUnShift);
+		setSoValue(target.BUTTON_UPP, 'text', strUp);
+		setSoValue(target.BUTTON_DWN, 'text', strDown);
 		return;
 	}
 
@@ -2425,11 +2447,9 @@ var tmp = function () {
 		n = -1;
 		if (shifted) {
 			n = n + shiftOffset;
-			setSoValue(target.EVENTS_DIALOG['SHIFT'], 'text', 'unSh');
-			//setSoValue(target.EVENTS_DIALOG.SHIFT, 'text', '\u2193'); // down arrow
+			setSoValue(target.EVENTS_DIALOG.SHIFT, 'text', strUnShift);
 		} else {
-			setSoValue(target.EVENTS_DIALOG['SHIFT'], 'text', 'Shft');
-			//setSoValue(target.EVENTS_DIALOG.SHIFT, 'text', '\u2191'); // up arrow
+			setSoValue(target.EVENTS_DIALOG.SHIFT, 'text', strShift);
 		}
 		if (symbols) {
 			n = n + symbolsOffset;
@@ -2480,8 +2500,8 @@ var tmp = function () {
 	target.addCharacter = function (id) {
 		var n = parseInt(id.substring(3, 5));
 		//target.bubble("tracelog","id="+id+", n="+n);
-		if (symbols) { n = n + symbolsOffset};
-		if (shifted) { n = n + shiftOffset};
+		if (symbols) { n = n + symbolsOffset };
+		if (shifted) { n = n + shiftOffset };
 		var character = keys[n-1];
 		//target.bubble("tracelog","n="+n+", character="+character);
 		var eventDescription = target.getVariable("event_description");
