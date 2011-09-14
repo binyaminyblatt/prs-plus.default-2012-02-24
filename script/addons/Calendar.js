fb9f07630a665d403f392e9454d46e1ea63a9db0
kartu <kartu@localhost>
2011-09-14 23:02:42 +0200

    Minor fixes for 2.0.13beta

--- script/addons/Calendar.js
+++ script/addons/Calendar.js
@@ -9,7 +9,6 @@ tmp = function() {
 		name: "Calendar",
 		title: "Calendar",
 		description: "Calendar app",
-		comment: "and Events",
 		icon: appIcon,			
 		activate: function () {
 			kbook.autoRunRoot.sandbox._icon =  Core.config.compat.NodeKinds.getIcon(appIcon,0);
