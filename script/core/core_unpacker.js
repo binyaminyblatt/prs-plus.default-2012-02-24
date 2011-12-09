6256d0b331e40adee86452dc58e71139cce26521
BenChen <ben.chenoweth@gmail.com>
2011-12-09 12:01:05 +0400

    Unpacker: Reinstated unrar

--- script/core/core_unpacker.js
+++ script/core/core_unpacker.js
@@ -5,11 +5,12 @@
 // History:
 //	2011-08-09 Shura1oplot - Initial version
 //  2011-12-08 Ben Chenoweth - Added CBR and CBZ to supported archives; commented out unrar for now
+//  2011-12-09 Ben Chenoweth - Reinstated unrar
 //
 try {
 	tmp = function() {
 		var L, supportedArchives, defaultPassword, UNRAR, P7ZIP, unpack, getRarCmdLine, getP7zipCmdLine;
-		//UNRAR = System.applyEnvironment("[prspPath]") + "unrar";
+		UNRAR = System.applyEnvironment("[prspPath]") + "unrar";
 		P7ZIP = System.applyEnvironment("[prspPath]") + "7za";
 		supportedArchives = {
 			"7z": true,
@@ -21,19 +22,18 @@ try {
 		defaultPassword = "qwerty";
 
 		unpack = function (path, outputDir, password) {
-			//var extension;
+			var extension;
 			
 			if (password === undefined) {
 				password = defaultPassword;
 			}
 			
-			// 7za is supposed to be able to handle rar files, so these lines have been commented out for now
-			//extension = Core.io.extractExtension(path);
-			//if ((extension === "rar") || (extension === "cbr")) {
-				//cmd = getRarCmdLine(path, outputDir, password);
-			//} else {
+			extension = Core.io.extractExtension(path);
+			if ((extension === "rar") || (extension === "cbr")) {
+				cmd = getRarCmdLine(path, outputDir, password);
+			} else {
 				cmd = getP7zipCmdLine(path, outputDir, password);
-			//}
+			}
 			try {
 				//log.trace(cmd);
 				Core.shell.exec(cmd);
@@ -42,15 +42,16 @@ try {
 			}
 		};
 		
-		/*getRarCmdLine = function (path, outputDir, password) {
+		getRarCmdLine = function (path, outputDir, password) {
 			var cmdOutputDir;
 			if (outputDir === undefined) {
 				cmdOutputDir = "";
 			} else {
 				cmdOutputDir = " \"" + outputDir + "\"";
 			}
-			return UNRAR + " x -p\"" + password  + "\" -y \"" + path + "\"" + cmdOutputDir;
-		}; */
+			//return UNRAR + " x -p\"" + password  + "\" -y \"" + path + "\"" + cmdOutputDir;
+			return UNRAR + " x -y \"" + path + "\"" + cmdOutputDir;
+		};
 		
 		getP7zipCmdLine = function (path, outputDir, password) {
 			var cmdOutputDir;
